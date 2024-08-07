812df5b8cc4f0aa1962d6d91a7329b1a41f8226c
xsalles <xs.salles@gmail.com>
2024-08-06 21:44:01 -0300

    feat: created login component with a title(h1) and with input to put a information, like your username, and after you put your username, you can click in button, and was created a function called handleLoggin when you click in button, it's confirmed your loggin and change the state to isLogged: true.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,32 @@
+import { useState } from "react"
+import { login } from "../actions"
+import { useDispatch } from "react-redux"
 
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const dispatch = useDispatch();
+
+    const handleLoggin = () => {
+        dispatch(login({name: username}))
+    }
+
+
+return (
+    <div>
+        <h1>Faça o login</h1>
+
+        <input type="text" value={username}
+        onChange={(e) => setUsername(e.target.value)} 
+        placeholder="Coloque o seu username"/>
+
+        <button onClick={handleLoggin}>Confirme o seu login</button>
+    </div>
+)
+
+};
+
+
+export default Login;
 
 
 
